9301cdd72073cc972b5dd2c65186fca44cb6515e
Jędrzej Nowak <JedrzejNowak@protonmail.com>
2022-03-31 04:22:40 +0200

    Seting up application with Spring Initializer

---
+++ src/main/java/com/github/nowakjd/checkme/CheckmeApplication.java
@@ -0,0 +1,13 @@
+package com.github.nowakjd.checkme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nowakjd/checkme/CheckmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nowakjd.checkme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
